91ff4825365932f485bd691a006755fb4702a2f8
bkrem600 <61847345+bkrem600@users.noreply.github.com>
2020-10-16 20:29:52 +0100

    Create Company.java

---
+++ Step1/Company.java
@@ -0,0 +1,46 @@
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class Company {
+
+	private String name;
+	private LinkedList<Project> projects = new LinkedList<>();
+	
+	public Company(String name) {
+		this.name = name;
+	}
+
+	public void addProject(Project project) {
+		this.projects.add(project);
+	}
+	
+	public Boolean removeProject (Project project) {
+		return this.projects.remove(project);
+	}
+
+	public Project findProject (String title) {
+		for (Project project: projects) {
+			if (project.getTitle().equalsIgnoreCase(title)) {
+				return project;
+			}
+		}
+		return null;
+	}
+	
+	public void displayProject (Project project) {
+		System.out.println(project.toString());
+	}
+
+	public void displayAllProjects() {
+		if (projects.isEmpty()) {
+			System.out.println("Project list is empty");
+		} else {
+			Collections.sort(projects);
+			for (Project project: projects) {
+				displayProject(project);
+			}
+		}
+	}
+
+}

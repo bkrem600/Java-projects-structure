ad376ff0e15a4697801351e4bdb5969fb131fdb2
bkrem600 <61847345+bkrem600@users.noreply.github.com>
2020-10-16 20:32:09 +0100

    Create BinaryTree.java

---
+++ Step 3/BinaryTree.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class BinaryTree<T extends Comparable<T>> {
+
+	private TreeNode<T> root;
+
+	public boolean add(T data) {
+		if (root == null) {
+			root = new TreeNode<>(data);
+			return true;
+		}
+		return root.insert(data);
+	}
+
+	public void remove(T data) {
+		this.root = root.remove(root, data);
+	}
+
+	public String toString() {
+		if (root != null) {
+			return root.toString();
+		} else {
+			return null;
+		}
+	}
+
+    public ArrayList<T> inOrderTraversal() {
+        ArrayList<T> inOrder = new ArrayList<>();
+        inOrderHelper(root, inOrder);
+        return inOrder;
+    }
+
+    private void inOrderHelper(TreeNode<T> node, ArrayList<T> inOrder) {
+        if (node == null) {
+			return;
+        }
+        inOrderHelper(node.getLeftNode(), inOrder);
+		inOrder.add(node.getData());
+        inOrderHelper(node.getRightNode(), inOrder);
+	}
+
+	public T find(T data) {
+		return root.find(data);
+	}
+
+}
